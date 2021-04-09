079a992fa5453170dd49094a4f1ff06a956f09a7
Mateus Peixoto <mateuspeixoto.contato@gmail.com>
2021-04-09 03:57:46 -0300

    implementação de put e delete de temas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaComponent } from './tema/tema.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
-import { TemaComponent } from './tema/tema.component';
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
 
-  {path: '', redirectTo: 'entrar', pathMatch:  'full' },
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
-  {path:'entrar',component: EntrarComponent},
-  {path:'cadastrar',component: CadastrarComponent},
-  
-  {path:'inicio', component: InicioComponent},
-  {path:'tema', component: TemaComponent}
+  {path:'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent},
+
+  {path: 'tema-edit/:id', component: TemaEditComponent},
+  {path: 'tema-delete/:id', component: TemaDeleteComponent}
+
 ];
 
 @NgModule({
